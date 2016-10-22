32b6100188a64d554677aee52b679b5092ced63c
Sebastian Saier <sebastian.saier@web.de>
2016-10-22 18:01:19 +0200

    implemented leaf animation

--- app/_assets/javascripts/components/leaf-animation.js
+++ app/_assets/javascripts/components/leaf-animation.js
@@ -3,12 +3,13 @@ import $ from 'jquery';
 
 /* Leaf Animation -------------------- */
 
-let density = 30,
+let density = 26,
   speed = 0.6,
-  winHeight = $('.header').height(),
-  end = {yMin:-25, yMax:-25, xMin:820, xMax:1450, scaleMin:0.5, scaleMax:0.6, opacityMin:0, opacityMax:0},
-  mid = {yMin:-500, yMax:-400, xMin:350, xMax:450, scaleMin:0.6, scaleMax:0.6, opacityMin:0.1, opacityMax:0.25},
-  start = {yMin:-790, yMax:-850, xMin:-150, xMax:-100, scaleMin:0.4, scaleMax:0.6, opacityMin:0.05, opacityMax:0.15};
+  winWidth = $(window).width(),
+  winHeight = $(window).height(),
+  end = {yMin:-winHeight/50-25, yMax:-winHeight/50-25, xMin:600, xMax:750, scaleMin:0.3, scaleMax:0.6, opacityMin:0, opacityMax:0},
+  mid = {yMin:-winHeight/50-500, yMax:-winHeight/50-400, xMin:220, xMax:600, scaleMin:0.3, scaleMax:0.6, opacityMin:0.1, opacityMax:0.25},
+  start = {yMin:-winHeight/50-790, yMax:-winHeight/50-900, xMin:-100, xMax:-20, scaleMin:0.3, scaleMax:0.6, opacityMin:0.05, opacityMax:0.15};
 
 export function range(map, prop) {
   let min = map[prop + "Min"],
@@ -25,7 +26,7 @@ export function spawn(particle) {
   TweenLite.set(particle, {y:range(start, "y"), x:range(start, "x"), scale:range(start, "scale"), opacity:range(start, "opacity"), visibility:"hidden"});
 
   //the y tween should be continuous and smooth the whole duration
-  TweenLite.to(particle, wholeDuration, {delay:delay, rotation: 720, y:range(end, "y"), ease:Linear.easeNone});
+  TweenLite.to(particle, wholeDuration, {delay:delay, rotation: '+=900', y:range(end, "y"), ease:Linear.easeNone});
 
   //now tween the x independently so that it looks more randomized (rather than linking it with scale/opacity changes too)
   TweenLite.to(particle, partialDuration, {delay:delay, x:range(mid, "x"), ease:Power1.easeOut});
@@ -44,10 +45,12 @@ function randomIntFromInterval(min,max)
 
 $(window).ready(function() {
   console.log($(window).height());
-  let body = $(".header"),
+  if ($(window).width() > 760) {
+    let body = $(".header"),
     i, particle;
-  for (i = 0; i < density; i++) {
-    // spawn( $("<div />", {id:"particle"+i}).addClass("i-particle").appendTo('header') );
+    for (i = 0; i < density; i++) {
+      spawn( $("<div />", {id:"particle"+i}).addClass("i-particle").appendTo('header') );
+    }
   }
 });
 
